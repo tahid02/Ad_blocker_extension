69469f5dc3deb61ce85ed930d668b15312078c57
tahid02 <tanzim1463@gmail.com>
2024-05-14 01:38:19 +0600

    mv3 and using declarativenetrequest

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,22 +1,24 @@
 // IMPORTANT: Find the ad serving urls here : https://easylist.to/easylist/easylist.txt
 
-chrome.webRequest.onBeforeRequest.addListener(
-  (details) => {
-    const url = details.url;
-    const filters = ["googleadservices", "googlesyndication", "g.doubleclick"];
-    for (const filter of filters) {
-      if (url.indexOf(filter) != -1) {
-        return {
-          cancel: true,
-        };
-      }
-    }
-    return {
-      cancel: false,
-    };
-  },
-  {
-    urls: ["<all_urls>"],
-  },
-  ["blocking"]
-);
+// chrome.webRequest.onBeforeRequest.addListener(
+//   (details) => {
+//     const url = details.url;
+//     const filters = ["googleadservices", "googlesyndication", "g.doubleclick"];
+//     for (const filter of filters) {
+//       if (url.indexOf(filter) != -1) {
+//         return {
+//           cancel: true,
+//         };
+//       }
+//     }
+//     return {
+//       cancel: false,
+//     };
+//   },
+//   {
+//     urls: ["<all_urls>"],
+//   },
+//   ["blocking"]
+// );
+
+// UPDATED IN RULES.JSON
