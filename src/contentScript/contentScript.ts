deee8a82d60f6d27c789f37056a05ab9a7ff7575
tahid02 <tanzim1463@gmail.com>
2024-05-14 01:43:03 +0600

    block ad using contentscript

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,1 +1,29 @@
 // TODO: content script
+
+// Blocking ad using contentscript only
+// Method 01: by specifying website
+const rules: {
+  [url: string]: () => void;
+} = {
+  "https://www.nytimes.com/section/technology": filterNYTTechnology2,
+};
+
+function filterNYTTechnology() {
+  const app = document.getElementById("site-content");
+  const wrapper = document.getElementById("top-wrapper");
+  app.removeChild(wrapper);
+}
+
+// Method 02:
+function filterNYTTechnology2() {
+  const divs = document.getElementsByTagName("div");
+  for (const div of divs) {
+    if (div.className.indexOf("ad") != -1) {
+      div.style.display = "none";
+    }
+  }
+}
+/////////////////////////// For both function /////////////////////////
+if (document.URL in rules) {
+  rules[document.URL]();
+}
